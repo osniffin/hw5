22c61db8056329a70f8ab1334f355074f3ca2105
Olivia Sniffin <osniffin@usc.edu>
2025-04-15 18:28:15 +0000

    wordle and schedwork upload

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,7 +22,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
-
+bool fillScheduleBacktrack(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<size_t>& shiftsPerWorker, size_t day, size_t slot);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -36,10 +36,53 @@ bool schedule(
         return false;
     }
     sched.clear();
+    
     // Add your code below
+        
+    size_t n = avail.size();    //number of days
+    size_t k = avail[0].size(); //number of workers 
 
+    sched.resize(n);
+    sched = DailySchedule(n,std::vector<Worker_T>());
+    std::vector<size_t> shiftsPerWorker(k,0);
 
-
+    return fillScheduleBacktrack(avail, dailyNeed, maxShifts, sched, shiftsPerWorker, 0, 0);
 
 }
 
+bool fillScheduleBacktrack(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<size_t>& shiftsPerWorker, size_t day, size_t slot)
+{
+    size_t n = avail.size();    //number of days
+    size_t k = avail[0].size(); //number of workers 
+
+    if(day == n) return true; //Base Case: all days scheduled
+
+    if(slot==dailyNeed) //Case 1: all slots are filled for given day --> move to next day 
+    {
+        return fillScheduleBacktrack(avail, dailyNeed, maxShifts, sched, shiftsPerWorker, day + 1, 0);
+    }
+    //test every possible worker to see if they can work the given slot on the given day 
+    for(size_t i=0; i<k; i++)
+    {
+        //check if worker is available 
+        //& worker not scheduled for this day 
+        //& worker doesn't have maxShifts
+        std::vector<Worker_T>::iterator it = std::find(sched[day].begin(), sched[day].end(),i);
+        if(avail[day][i] && shiftsPerWorker[i]<maxShifts && it == sched[day].end() ){
+            sched[day].push_back(i); // add the worker to that day
+            shiftsPerWorker[i]++; //add 1 shift to given workers number of shifts
+            //recurse to fill the next slot 
+            if(fillScheduleBacktrack(avail, dailyNeed, maxShifts, sched, shiftsPerWorker, day, slot+1))
+            {
+                return true; //If it works, continue to the next slot
+            }
+            //Backtrack if it doesnt work - undo 
+            sched[day].pop_back();
+            shiftsPerWorker[i]--;
+        
+        }
+
+    }
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& results, size_t idx);
 
 
 // Definition of primary wordle function
@@ -22,7 +23,59 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    // in - word pattern with fixed letters and blanks
+    // floating - "yellow" letters that appear somewhere
+    // dict - dict-eng.txt
+    // things to keep track of: index, 
+  std::set<std::string> results; 
+  wordleHelper(in, floating, dict, results,0);
+  return results;
 }
 
+
 // Define any helper functions here
+void wordleHelper(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& results, size_t idx)
+{
+  if(idx == in.size())
+  {
+    if(floating.empty() && dict.find(in)!= dict.end())
+    {
+      results.insert(in);
+    }
+    return;
+  }
+  if(in[idx]!= '-')
+  {
+    wordleHelper(in,floating, dict, results, idx+1);
+
+  }else{ //index reached is - (blank)
+    // use a floating letter
+    // use any other letter a-z
+    //keep track of floating letters: only used once
+    //try a-z
+    for(char i='a';i<='z';i++){
+      std::string tempIn = in;
+      std::string newFloating = floating;
+
+      if(floating.find(i)!=std::string::npos){//Case 1: if 'i' is in floating letters
+        tempIn[idx]=i; //add the floating letter into the blank spot
+        newFloating.erase(floating.find(i),1); //erase the used floating value from the temp floating list
+        wordleHelper(tempIn,newFloating, dict, results, idx+1); //recurse
+
+      }else{// Case 2: if 'i' is NOT in floating
+        //if there are more dashes left than floating letters
+        //count blanks left 
+        size_t blanksLeft =0;
+        for (size_t j = 0; j < tempIn.size(); j++){
+          if(in[j] == '-') blanksLeft++;
+        }
+        //compare blanks to the new floating 
+        if(blanksLeft>newFloating.size()){ //if there are more blanks than floating letters remaining 
+          tempIn[idx]=i; //add the letter into the blank spot
+          wordleHelper(tempIn,newFloating, dict, results, idx+1); //recurse 
+        }
+      }
+    }
+  }
+}
+
